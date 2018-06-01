5a22d9eee642430e18084acf87c7c745517d74fa
Butan <c15487922@mydit.ie>
2018-06-01 08:47:06 +0200

    init

---
+++ Lab2.2/myscanner.h
@@ -0,0 +1,11 @@
+#define TYPE 1
+#define NAME 2
+#define TABLE_PREFIX 3
+#define PORT 4
+#define SOURCE 5
+// #define PACKAGE 6
+// #define IMPORTS 7
+#define COLON 6
+//#define OR 7
+#define IDENTIFIER 7
+#define INTEGER 8
